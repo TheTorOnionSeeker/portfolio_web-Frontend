b566083fdf5a4743a4967fc81b8b6a22a2ae5c6d
unknown <guillermooscarn@gmail.com>
2023-01-18 12:53:23 -0300

    added component Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './1985052.svg';
-import './App.css';
 import React, { useState } from 'react';
+import './App.css';
+import { Route,Link } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Click on my name to see more about me.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Guillermo Oscar Núñez
-        </a>
-      </header>
-    </div>
+    <>
+    <Route exact path={"/"} component={Home} />
+    </>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import logo from '../1985052.svg';
+import '../App.css';
+
+import { Route,Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Click on my name to see more about me.
+          </p>
+          <Link className="App-link" to={"/home"}>Guillermo Oscar Núñez</Link>
+        </header>
+      </div>
+    </>
+  )
+}
